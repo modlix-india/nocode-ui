7ca1f7513dc39e02d42f314f95b9b3e2da442983
akhilesh-fincity <akhilesh.a@fincity.com>
2022-10-14 15:24:15 +0530

    added edge case to translations and added setLanguage json structure in appData

--- ui-app/client/src/components/util/getTranslations.ts
+++ ui-app/client/src/components/util/getTranslations.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import { getData } from '../../context/StoreContext';
 
 export function getTranslations(key : string, languageObject: { [key: string] : { [key: string] : string } }){
+    if(!languageObject) return key;
     const lang = getData('Store.currentLanguage')??'en';
     if(!languageObject[lang]) return key;
    return languageObject[lang][key]??key;
