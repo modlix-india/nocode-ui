a324b22d0caaf529ae3aac35fa9ca687205bf8be
akhilesh-fincity <akhilesh.a@fincity.com>
2023-02-06 14:37:25 +0530

    added style properties

--- ui-app/client/src/components/Tabs/TabsStyle.tsx
+++ ui-app/client/src/components/Tabs/TabsStyle.tsx
@@ -6,52 +6,15 @@ const PREFIX = '.comp.compTabs';
 export default function TabsStyles({ theme }: { theme: Map<string, Map<string, string>> }) {
 	const css =
 		`
-${PREFIX}.compTabs {
-	margin-left: 20px;
-	margin-right:20px
-}
+		${PREFIX} .tabsButtons {
+			letter-spacing: 0px;
+		}
 
-${PREFIX} .tabsButtons {
-	background-color:#ffffff;
-	border: none;
-	text-align: left;
-	font-size:  15px;
-	font-family: Open Sans;
-	letter-spacing: 0px;
-	color: #6c7586;
-	opacity: 1;
-	display: flex;
-	gap:3px;
-	justify-content: center;
-	align-items: center;
-}
+		${PREFIX} .tabsButtonActive {
+			letter-spacing: 0px;
+		}
 
-${PREFIX} .tabsButtonActive {
-	background-color:#ffffff;
-	border: none;
-	text-align: left;
-	font-size:  15px;
-	font-family: Open Sans;
-	letter-spacing: 0px;
-	color: #393a3c;
-	opacity: 1;
-	display: flex;
-	border-bottom: 6px solid #1f3c3d;
-	gap:3px;
-	justify-content: center;
-	align-items: center;
-}
-${PREFIX} .tabButtonDiv {
-	width: 100%;
-	justify-content: space-between;
-	display: flex;
-	padding-left: 20px;
-    padding-right: 20px;
-	border-bottom: 1px solid #cecece;
-}
-${PREFIX} .tabBorder {
-	border-bottom: 1px solid #cecece;
-}
+
 
 ` + processStyleDefinition(PREFIX, styleProperties, styleDefaults, theme);
 
