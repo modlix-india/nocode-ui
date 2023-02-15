3a486eaca551f0ee7c4a4614ae8ec9e12a3fafa5
serialcoder13 <kiran.grandhi@fincity.com>
2023-02-15 23:28:08 +0530

    Added default style to page

--- ui-app/client/src/components/Page/PageStyle.tsx
+++ ui-app/client/src/components/Page/PageStyle.tsx
@@ -4,7 +4,9 @@ import { styleProperties, styleDefaults } from './pageStyleProperties';
 
 const PREFIX = '.comp.compPage';
 export default function PageStyle({ theme }: { theme: Map<string, Map<string, string>> }) {
-	const css = `` + processStyleDefinition(PREFIX, styleProperties, styleDefaults, theme);
+	const css =
+		`${PREFIX} { display: flex; flex-direction: column }` +
+		processStyleDefinition(PREFIX, styleProperties, styleDefaults, theme);
 
 	return <style id="PageCss">{css}</style>;
 }
