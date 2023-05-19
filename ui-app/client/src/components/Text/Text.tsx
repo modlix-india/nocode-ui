fffb60cad1d3d53eb185fce2719d9ef19388ee16
serialcoder13 <kiran.grandhi@fincity.com>
2023-05-19 22:43:43 +0530

    Added String format for date which can be used later for calendar and time

--- ui-app/client/src/components/Text/Text.tsx
+++ ui-app/client/src/components/Text/Text.tsx
@@ -9,6 +9,7 @@ import TextStyle from './TextStyle';
 import useDefinition from '../util/useDefinition';
 import { processComponentStylePseudoClasses } from '../../util/styleProcessor';
 import { SubHelperComponent } from '../SubHelperComponent';
+import { formatString } from '../../util/stringFormat';
 
 function Text(props: ComponentProps) {
 	const {
@@ -19,7 +20,14 @@ function Text(props: ComponentProps) {
 	} = props;
 	const pageExtractor = PageStoreExtractor.getForContext(context.pageName);
 	const {
-		properties: { text: originalTextObj, textContainer, textType, processNewLine } = {},
+		properties: {
+			text: originalTextObj,
+			textContainer,
+			textType,
+			processNewLine,
+			stringFormat,
+			textLength,
+		} = {},
 		stylePropertiesWithPseudoStates,
 	} = useDefinition(
 		definition,
@@ -44,6 +52,7 @@ function Text(props: ComponentProps) {
 			: originalTextObj;
 
 	let translatedText = getTranslations(text, translations);
+	let originalText = translatedText;
 
 	if (textType === 'MD') {
 		return (
@@ -52,6 +61,20 @@ function Text(props: ComponentProps) {
 				<MarkDown text={translatedText ?? ''} />
 			</div>
 		);
+	}
+
+	if (stringFormat !== 'STRING' && translatedText) {
+		translatedText = formatString(translatedText, stringFormat);
+	}
+
+	if (textLength && translatedText) {
+		let finTextLength = textLength - 3;
+		if (finTextLength < 3) finTextLength = 3;
+		const ellipsis = finTextLength === 3 ? '' : '...';
+		translatedText =
+			translatedText.length > finTextLength
+				? translatedText.substring(0, finTextLength) + ellipsis
+				: translatedText;
 	}
 
 	let comps: React.ReactNode[] = [];
@@ -80,7 +103,7 @@ function Text(props: ComponentProps) {
 		...comps,
 	);
 	return (
-		<div className="comp compText" style={styleProperties.comp ?? {}}>
+		<div className="comp compText" style={styleProperties.comp ?? {}} title={originalText}>
 			<HelperComponent definition={definition} />
 			{comp}
 		</div>
