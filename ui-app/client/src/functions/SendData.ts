f55418f3a3d0e92e96d80a365db0ae0026f9c86c
raja-avinash <raja.avinash@fincity.com>
2023-06-01 13:03:45 +0530

    changes for null value and updated kirun verison

--- ui-app/client/src/functions/SendData.ts
+++ ui-app/client/src/functions/SendData.ts
@@ -106,9 +106,10 @@ export class SendData extends AbstractFunction {
 			ll.add(payload);
 			while (ll.size() > 0) {
 				const current = ll.pop();
+				console.log(current, ll);
 				if (Array.isArray(current)) {
 					ll.addAll([...current]);
-				} else if (typeof current === 'object') {
+				} else if (typeof current === 'object' && current !== null) {
 					if (current.constructor?.name === 'File') {
 						isFormData = true;
 						break;
