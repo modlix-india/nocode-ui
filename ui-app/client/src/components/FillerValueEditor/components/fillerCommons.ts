d9217fbacd628f802831b17b16bf1c087a850fcc
serialcoder13 <kiran.grandhi@fincity.com>
2023-12-12 21:43:05 +0530

    Added
    1. Changed the color of the dotted highlight to Blue.
    2. Along with the list of grid keys to scroll to, added an option to choose where the scrolled component should be.
    3. Close icon added for the popups and image popup.
    4. Added Map editor for adding map and image.
    5. Page refresh when there is no scroll grid is specified.
    6. Re arrangement of the gallery images in the array is done.

--- ui-app/client/src/components/FillerValueEditor/components/fillerCommons.ts
+++ ui-app/client/src/components/FillerValueEditor/components/fillerCommons.ts
@@ -9,6 +9,7 @@ export enum EditorType {
 	PALLETTE = 'PALLETTE',
 	LABEL = 'LABEL',
 	FONT_PICKER = 'FONT_PICKER',
+	MAP = 'MAP',
 }
 
 export interface EditorDefinition {
@@ -28,6 +29,9 @@ export interface EditorDefinition {
 	objectEditors?: EditorDefinition[];
 	arrayPreviewList?: string[];
 	arrayPreviewType?: 'LIST' | 'GRID';
+	samplePalettes?: { displayName: string; colors: string[] }[];
+	hideGeneratePalette?: boolean;
+	mapURLPrefix?: string;
 }
 
 export enum SectionLayout {
