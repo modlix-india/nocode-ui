9fdb2f9ee8ef7b2af7f92828f9921676bf994085
serialcoder13 <kiran.grandhi@fincity.com>
2024-02-27 16:15:34 +0530

    Working on the chart legend labels.

--- ui-app/client/src/components/Chart/types/chartComponents/Grid.tsx
+++ ui-app/client/src/components/Chart/types/chartComponents/Grid.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import { ChartData, ChartProperties } from '../common';
 
-export default function Grid({}: Readonly<{
+export default function Grid({
+	properties,
+	chartData,
+}: Readonly<{
 	properties: ChartProperties;
 	chartData: ChartData;
 }>) {
+	if (properties.hideGrid) return <></>;
+
+	if (properties.chartType === 'waffle') return <></>;
+
+	let dataSetStyle = [];
+	if (chartData.dataSetStyles.length === 0) {
+		switch (properties.chartType) {
+			case 'regular':
+				dataSetStyle = Array(chartData.yAxisData.length).fill('line');
+				break;
+
+			case 'radial':
+				dataSetStyle = Array(chartData.yAxisData.length).fill('pie');
+				break;
+
+			case 'radar':
+				dataSetStyle = Array(chartData.yAxisData.length).fill('radar');
+				break;
+
+			case 'dot':
+				dataSetStyle = Array(chartData.yAxisData.length).fill('dot');
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	const centerLabel = !dataSetStyle.some(style => style === 'bar');
+
+	if (!properties.hideXAxis) {
+	}
+
 	return <></>;
 }
