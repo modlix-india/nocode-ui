473fda606e88561309716155282fa6e6e11e6fbd
Venkataraochavala <c.venkatarao@fincity.com>
2024-05-28 16:06:20 +0530

    fixed restrict upload type issue

--- ui-app/client/src/commonComponents/FileBrowser/index.tsx
+++ ui-app/client/src/commonComponents/FileBrowser/index.tsx
@@ -57,7 +57,7 @@ interface FileBrowserProps {
 	restrictNavigationToTopLevel?: boolean;
 	startLocation?: string;
 	fileUploadSizeLimit?: number;
-	restrictUploadType?: string;
+	restrictUploadType?: string[];
 	restrictSelectionType?: string[];
 	selectionType?: string;
 	hideUploadFile?: boolean;
@@ -155,7 +155,6 @@ export function FileBrowser({
 	const [fileSelection, setFileSelection] = useState<any>();
 	const [somethingChanged, setSomethingChanged] = useState(Date.now());
 	const [deleteObject, setDeleteObject] = useState<any>();
-
 	const setPathAndClearFilter = useCallback(
 		(v: string) => {
 			setPath(v);
@@ -252,12 +251,13 @@ export function FileBrowser({
 						alert('File size exceeds the limit');
 						return;
 					}
-
-					const extension = file.name.split('.').pop()?.toLowerCase();
+					//console.log('filename ', file, file?.name);
+					const extension = file?.name?.split('.').pop()?.toLowerCase();
 					if (
 						restrictUploadType &&
-						!restrictUploadType.split(',').includes(extension ?? '')
+						!restrictUploadType.includes(extension ?? '')
 					) {
+						//console.log('file type not valid');
 						alert(
 							'File type not allowed, please upload only of type ' +
 								restrictUploadType,
@@ -739,7 +739,6 @@ function isSelectable(
 
 	if (selectionType === '_files' && file.directory) return false;
 	else if (selectionType === '_folders' && !file.directory) return false;
-
 	return true;
 }
 
