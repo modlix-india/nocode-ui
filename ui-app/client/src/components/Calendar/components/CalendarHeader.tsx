73a53e897a07e2d55fb8eeb53590868a74c69d0b
serialcoder13 <kiran.grandhi@fincity.com>
2024-06-17 13:24:07 +0530

    Added support for navigation of months.

--- ui-app/client/src/components/Calendar/components/CalendarHeader.tsx
+++ ui-app/client/src/components/Calendar/components/CalendarHeader.tsx
@@ -7,7 +7,15 @@ import {
 } from './calendarFunctions';
 import { CalendarAllProps } from './calendarTypes';
 
-export function CalendarHeader(props: CalendarAllProps) {
+export function CalendarHeader(
+	props: CalendarAllProps & {
+		onMonthClick: () => void;
+		onYearClick: () => void;
+		onBrowsingMonthYearChange: (value: string) => void;
+		onPreviousClick: () => void;
+		onNextClick: () => void;
+	},
+) {
 	const {
 		currentDate,
 		styles,
@@ -17,8 +25,6 @@ export function CalendarHeader(props: CalendarAllProps) {
 		leftArrowImage,
 		rightArrowImage,
 		arrowButtonsHorizontalPlacement,
-		calendarFormat,
-		monthLabels,
 	} = props;
 
 	const [hovers, setHovers] = React.useState<Set<string>>(() => new Set());
@@ -33,9 +39,10 @@ export function CalendarHeader(props: CalendarAllProps) {
 		<div
 			key="leftArrow"
 			className={`_leftArrow`}
-			style={getStyleObject('leftArrow', hovers, disableds) ?? {}}
+			style={getStyleObject('leftArrow', hovers, disableds)}
 			onMouseEnter={addToToggleSetCurry(hovers, setHovers, 'leftArrow')}
 			onMouseLeave={removeFromToggleSetCurry(hovers, setHovers, 'leftArrow')}
+			onClick={props.onPreviousClick}
 		>
 			<SubHelperComponent definition={definition} subComponentName="leftArrow" zIndex={7} />
 			{leftArrowImage ? <img src={leftArrowImage} /> : <ArrowRight rotate={180} />}
@@ -43,36 +50,192 @@ export function CalendarHeader(props: CalendarAllProps) {
 		<div
 			key="rightArrow"
 			className={`_rightArrow`}
-			style={getStyleObject('rightArrow', hovers, disableds) ?? {}}
+			style={getStyleObject('rightArrow', hovers, disableds)}
 			onMouseEnter={addToToggleSetCurry(hovers, setHovers, 'rightArrow')}
 			onMouseLeave={removeFromToggleSetCurry(hovers, setHovers, 'rightArrow')}
+			onClick={props.onNextClick}
 		>
 			<SubHelperComponent definition={definition} subComponentName="rightArrow" zIndex={7} />
 			{rightArrowImage ? <img src={rightArrowImage} /> : <ArrowRight rotate={0} />}
 		</div>,
 	];
 
-	let position = 0;
-	if (arrowButtonsHorizontalPlacement === '_right') position = 2;
+	let position = 2;
+	if (arrowButtonsHorizontalPlacement === '_right') position = 0;
 	else if (arrowButtonsHorizontalPlacement === '_either') position = 1;
-	header.splice(position, 0, <CalendarTitle key="title" {...props} />);
+	header.splice(
+		position,
+		0,
+		<div
+			key="title"
+			className="_calendarHeaderTitle"
+			style={getStyleObject('calendarHeaderTitle', hovers, disableds)}
+			onMouseEnter={addToToggleSetCurry(hovers, setHovers, 'calendarHeaderTitle')}
+			onMouseLeave={removeFromToggleSetCurry(hovers, setHovers, 'calendarHeaderTitle')}
+		>
+			<CalendarTitle {...props} />
+		</div>,
+	);
 
 	return (
 		<div
 			className="_calendarHeader"
-			style={getStyleObject('calendarHeader', hovers, disableds) ?? {}}
+			style={getStyleObject('calendarHeader', hovers, disableds)}
 		>
-			{header}
 			<SubHelperComponent definition={definition} subComponentName="calendarHeader" />
+			{header}
 		</div>
 	);
 }
 
-function CalendarTitle(props: CalendarAllProps) {
-	return <></>;
+function CalendarTitle(
+	props: CalendarAllProps & { onMonthClick: () => void; onYearClick: () => void },
+) {
+	if (props.calendarFormat === 'showCurrentYear') {
+		return (
+			<CalendarYearTitle
+				{...props}
+				year={props.currentDate.getFullYear()}
+				onClick={props.onYearClick}
+			/>
+		);
+	} else if (props.calendarFormat === 'showCurrentMonth') {
+		return (
+			<>
+				<CalendarMonthTitle
+					{...props}
+					month={props.currentDate.toLocaleDateString(props.language, {
+						month: props.monthLabels,
+					})}
+					onClick={props.onYearClick}
+				/>
+				<CalendarYearTitle
+					{...props}
+					year={props.currentDate.getFullYear()}
+					onClick={props.onYearClick}
+				/>
+			</>
+		);
+	}
+
+	let fromDate, toDate;
+
+	if (props.calendarFormat === 'showCurrentMonthAndNext') {
+		fromDate = props.currentDate;
+		toDate = new Date(fromDate);
+		toDate.setMonth(fromDate.getMonth() + 1);
+	} else if (props.calendarFormat === 'showCurrentMonthAndPrevious') {
+		fromDate = new Date(props.currentDate);
+		fromDate.setMonth(fromDate.getMonth() - 1);
+		toDate = props.currentDate;
+	} else if (props.calendarFormat === 'showPreviousCurrentAndNextMonth') {
+		fromDate = new Date(props.currentDate);
+		fromDate.setMonth(fromDate.getMonth() - 1);
+		toDate = new Date(props.currentDate);
+		toDate.setMonth(toDate.getMonth() + 1);
+	} else if (props.calendarFormat === 'showCurrentAndPreviousTwoMonths') {
+		fromDate = new Date(props.currentDate);
+		fromDate.setMonth(fromDate.getMonth() - 2);
+		toDate = props.currentDate;
+	} else if (props.calendarFormat === 'showCurrentAndNextTwoMonths') {
+		fromDate = props.currentDate;
+		toDate = new Date(props.currentDate);
+		toDate.setMonth(toDate.getMonth() + 2);
+	} else if (props.calendarFormat === 'showFourMonths') {
+		fromDate = new Date(props.currentDate);
+		fromDate.setMonth(fromDate.getMonth() - 2);
+		toDate = new Date(props.currentDate);
+		toDate.setMonth(toDate.getMonth() + 1);
+	} else if (props.calendarFormat === 'showSixMonths') {
+		fromDate = new Date(props.currentDate);
+		fromDate.setMonth(fromDate.getMonth() - 2);
+		toDate = new Date(props.currentDate);
+		toDate.setMonth(toDate.getMonth() + 3);
+	} else if (props.calendarFormat === 'showTwelveMonths') {
+		fromDate = new Date(props.currentDate);
+		fromDate.setMonth(fromDate.getMonth() - 5);
+		toDate = new Date(props.currentDate);
+		toDate.setMonth(toDate.getMonth() + 6);
+	} else {
+		throw new Error('Invalid calendar format');
+	}
+
+	return (
+		<>
+			<CalendarMonthTitle
+				{...props}
+				month={fromDate.toLocaleDateString(props.language, {
+					month: props.monthLabels,
+				})}
+				onClick={props.onMonthClick}
+			/>
+			<CalendarYearTitle
+				{...props}
+				year={fromDate.getFullYear()}
+				onClick={props.onYearClick}
+			/>
+			-
+			<CalendarMonthTitle
+				{...props}
+				month={toDate.toLocaleDateString(props.language, {
+					month: props.monthLabels,
+				})}
+				onClick={props.onMonthClick}
+			/>
+			<CalendarYearTitle {...props} year={toDate.getFullYear()} onClick={props.onYearClick} />
+		</>
+	);
 }
 
-function ArrowRight({ rotate }: { rotate: number }) {
+function CalendarYearTitle(props: CalendarAllProps & { year: number; onClick: () => void }) {
+	const [hover, setHover] = React.useState<boolean>(false);
+
+	const yearStyles = getStyleObjectCurry(props.styles, props.hoverStyles, props.disabledStyles)(
+		'yearNumber',
+		hover ? new Set(['yearNumber']) : new Set(),
+		new Set(),
+	);
+
+	return (
+		<div
+			className="_yearNumber"
+			style={yearStyles}
+			onMouseEnter={() => setHover(true)}
+			onMouseLeave={() => setHover(false)}
+			onClick={props.onClick}
+		>
+			<SubHelperComponent definition={props.definition} subComponentName="yearNumber" />
+			{props.year}
+		</div>
+	);
+}
+
+export function CalendarMonthTitle(
+	props: CalendarAllProps & { month: string; onClick?: () => void },
+) {
+	const [hover, setHover] = React.useState<boolean>(false);
+
+	const monthStyles = getStyleObjectCurry(props.styles, props.hoverStyles, props.disabledStyles)(
+		'monthName',
+		hover ? new Set(['monthName']) : new Set(),
+		new Set(),
+	);
+
+	return (
+		<div
+			className="_monthName"
+			style={monthStyles}
+			onMouseEnter={() => setHover(true)}
+			onMouseLeave={() => setHover(false)}
+			onClick={props.onClick}
+		>
+			<SubHelperComponent definition={props.definition} subComponentName="monthName" />
+			{props.month}
+		</div>
+	);
+}
+
+function ArrowRight({ rotate }: Readonly<{ rotate: number }>) {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
