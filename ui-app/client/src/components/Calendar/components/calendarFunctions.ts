b8e021d11b7f33518f1091813152c4cc504391f7
serialcoder13 <kiran.grandhi@fincity.com>
2024-06-20 20:12:00 +0530

    Forgot to set milli seconds in zero hours.

--- ui-app/client/src/components/Calendar/components/calendarFunctions.ts
+++ ui-app/client/src/components/Calendar/components/calendarFunctions.ts
@@ -234,6 +234,7 @@ export function zeroHourDate(date: Date | undefined): Date | undefined {
 	date.setHours(0);
 	date.setMinutes(0);
 	date.setSeconds(0);
+	date.setMilliseconds(0);
 	return date;
 }
 
