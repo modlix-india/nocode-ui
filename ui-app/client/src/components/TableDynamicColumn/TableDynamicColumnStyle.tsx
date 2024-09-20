7608ed5d1a4f8cdf2fdb0fccfda1f6ec0aa188ea
serialcoder13 <kiran.grandhi@fincity.com>
2024-09-20 22:01:55 +0530

    No Code required for Table Dynamic Column Style

--- ui-app/client/src/components/TableDynamicColumn/TableDynamicColumnStyle.tsx
+++ ui-app/client/src/components/TableDynamicColumn/TableDynamicColumnStyle.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { processStyleDefinition } from '../../util/styleProcessor';
-import { styleDefaults, styleProperties } from './tableDynamicColumnStyleProperties';
 
-
-const PREFIX = '.comp.compTableDynamicColumn';
 export default function TableDynamicColumnStyle({
 	theme,
 }: {
-	theme: Map<string, Map<string,string>>;
+	theme: Map<string, Map<string, string>>;
 }) {
-	const css = 
-	`${PREFIX} { display: table; flex-direction: column; flex:1; }
-	 ${PREFIX} ._row { display: table-row; }
-	` + processStyleDefinition(PREFIX,styleProperties,styleDefaults , theme);
-
-	return <style id="TableDynamicColumnCss">{css}</style>;
+	return <></>;
 }
-
-
