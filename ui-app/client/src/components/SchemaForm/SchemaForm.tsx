6b6d76ee05711d87c45399652b89ffb6a1647663
serialcoder13 <kiran.grandhi@fincity.com>
2024-12-15 15:28:28 +0530

    Lazy Loading schema form

--- ui-app/client/src/components/SchemaForm/SchemaForm.tsx
+++ ui-app/client/src/components/SchemaForm/SchemaForm.tsx
@@ -1,6 +1,6 @@
 import { deepEqual, duplicate, Repository, Schema } from '@fincity/kirun-js';
 import { setStoreData, StoreExtractor } from '@fincity/path-reactive-state-management';
-import React, { useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import {
 	addListenerAndCallImmediately,
 	getPathFromLocation,
@@ -19,97 +19,14 @@ import SchemaFormStyle from './SchemaFormStyle';
 import { styleDefaults } from './schemaFormStyleProperies';
 import { IconHelper } from '../util/IconHelper';
 
-function SchemaForm(
-	props: ComponentProps & {
-		schema?: Schema;
-		schemaRepository?: Repository<Schema>;
-		onChange?: (value: any) => void;
-		value?: any;
-	},
-) {
-	const {
-		definition,
-		definition: { bindingPath },
-		locationHistory,
-		context,
-	} = props;
-	const pageExtractor = PageStoreExtractor.getForContext(context.pageName);
-	const {
-		key,
-		properties: { schema: jsonSchema, design, readOnly, showJSONEditorButton } = {},
-		stylePropertiesWithPseudoStates,
-	} = useDefinition(
-		definition,
-		propertiesDefinition,
-		stylePropertiesDefinition,
-		locationHistory,
-		pageExtractor,
-	);
-
-	const bindingPathPath = bindingPath
-		? getPathFromLocation(bindingPath, locationHistory, pageExtractor)
-		: undefined;
-
-	const [value, setValue] = React.useState<any>(null);
-	useEffect(() => {
-		if (!bindingPathPath) {
-			setValue(props.value);
-			return;
-		}
-		return addListenerAndCallImmediately((_, v) => setValue(v), pageExtractor, bindingPathPath);
-	}, [bindingPathPath, props.value]);
-
-	const isReadonly = readOnly || (!bindingPathPath && !props.onChange);
-
-	const schema = React.useMemo(() => props.schema ?? Schema.from(jsonSchema), [jsonSchema]);
-
-	const resolvedStyles = processComponentStylePseudoClasses(
-		props.pageDefinition,
-		{},
-		stylePropertiesWithPseudoStates,
-	);
-	const schemaRepository = props.schemaRepository ?? UISchemaRepository;
-
+const LazySchemaForm = React.lazy(
+	() => import(/* webpackChunkName: "FillerDefinitionEditor" */ './LazySchemaForm'),
+);
+function LoadLazySchemaForm(props: Readonly<ComponentProps>) {
 	return (
-		<div className="comp compSchemaForm" style={resolvedStyles.comp ?? {}}>
-			<HelperComponent context={props.context} definition={definition} />
-			{showJSONEditorButton && (
-				<AnyValueEditor
-					value={value === undefined ? null : value}
-					onChange={v => {
-						if (isReadonly || deepEqual(v, value)) return;
-						if (bindingPathPath)
-							setData(bindingPathPath, v, pageExtractor.getPageName());
-						else if (props.onChange) props.onChange(v);
-					}}
-					isIconButton={true}
-				/>
-			)}
-			<SingleSchema
-				schema={schema}
-				path=""
-				value={value}
-				schemaRepository={schemaRepository}
-				onChange={(path, v) => {
-					const internal = { value: duplicate(value) };
-
-					const map = new Map([['Internal.', new StoreExtractor(internal, 'Internal.')]]);
-
-					setStoreData(
-						'Internal.value' + (path ? '.' + path : ''),
-						internal,
-						v,
-						'Internal',
-						map,
-					);
-
-					if (bindingPathPath) {
-						setData(bindingPathPath, internal.value, pageExtractor.getPageName());
-					}
-					props.onChange?.(internal.value);
-				}}
-			/>
-		</div>
+		<Suspense fallback={<>...</>}>
+			<LazySchemaForm {...props} />
+		</Suspense>
 	);
 }
 
@@ -117,7 +34,7 @@ const component: Component = {
 	name: 'SchemaForm',
 	displayName: 'Schema Form',
 	description: 'Schema Form component',
-	component: SchemaForm,
+	component: LoadLazySchemaForm,
 	propertyValidation: (props: ComponentPropertyDefinition): Array<string> => [],
 	properties: propertiesDefinition,
 	styleComponent: SchemaFormStyle,
