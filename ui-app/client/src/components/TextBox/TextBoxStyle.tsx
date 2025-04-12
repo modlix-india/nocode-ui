47580b9eb0df7abcbc86e9d89c595a683f3e39f8
serialcoder13 <kiran.grandhi@fincity.com>
2025-04-12 23:54:07 +0530

    Textbox is restructured

--- ui-app/client/src/components/TextBox/TextBoxStyle.tsx
+++ ui-app/client/src/components/TextBox/TextBoxStyle.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { processStyleDefinition } from '../../util/styleProcessor';
-import { styleProperties, styleDefaults } from './textBoxStyleProperties';
+import { styleProperties, styleDefaults, stylePropertiesForTheme } from './textBoxStyleProperties';
 import { usedComponents } from '../../App/usedComponents';
-import { StylePropertyDefinition } from '../../types/common';
-import { lazyStylePropertyLoadFunction } from '../util/lazyStylePropertyUtil';
+import {
+	findPropertyDefinitions,
+	lazyStylePropertyLoadFunction,
+} from '../util/lazyStylePropertyUtil';
+import { propertiesDefinition } from './textBoxProperties';
 
 const PREFIX = '.comp.compTextBox';
 const NAME = 'TextBox';
@@ -13,14 +16,29 @@ export default function TextBoxStyle({
 	const [_, setReRender] = useState<number>(Date.now());
 
 	if (globalThis.styleProperties[NAME] && !styleProperties.length && !styleDefaults.size) {
-		styleProperties.splice(0, 0, ...globalThis.styleProperties[NAME])
-		styleProperties.filter((e: any) => !!e.dv)?.map(
-			({ n: name, dv: defaultValue }: any) => styleDefaults.set(name, defaultValue),
-		);
+		styleProperties.splice(0, 0, ...globalThis.styleProperties[NAME]);
+		styleProperties
+			.filter((e: any) => !!e.dv)
+			?.map(({ n: name, dv: defaultValue }: any) => styleDefaults.set(name, defaultValue));
 	}
 
 	useEffect(() => {
-		const fn = lazyStylePropertyLoadFunction(NAME, (props) => { styleProperties.splice(0, 0, ...props); setReRender(Date.now()) }, styleDefaults);
+		const { designType, colorScheme } = findPropertyDefinitions(
+			propertiesDefinition,
+			'designType',
+			'colorScheme',
+		);
+		const fn = lazyStylePropertyLoadFunction(
+			NAME,
+			(props, originalStyleProps) => {
+				styleProperties.splice(0, 0, ...props);
+				if (originalStyleProps) stylePropertiesForTheme.splice(0, 0, ...originalStyleProps);
+				console.log(styleDefaults);
+				setReRender(Date.now());
+			},
+			styleDefaults,
+			[designType, colorScheme],
+		);
 
 		if (usedComponents.used(NAME)) fn();
 		usedComponents.register(NAME, fn);
