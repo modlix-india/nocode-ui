712bf6fb0170639611157bb80d758daafefeae09
RoopaSowmya14 <144905679+RoopaSowmya14@users.noreply.github.com>
2025-04-21 16:45:34 +0530

    added style props
    added style props

--- ui-app/client/src/components/ToggleButton/toggleButtonProperties.ts
+++ ui-app/client/src/components/ToggleButton/toggleButtonProperties.ts
@@ -138,6 +138,17 @@ const stylePropertiesDefinition: ComponentStylePropertyDefinition = {
 		COMPONENT_STYLE_GROUP_PROPERTIES.spacing.type,
 		COMPONENT_STYLE_GROUP_PROPERTIES.typography.type,
 	],
+	icon: [
+		COMPONENT_STYLE_GROUP_PROPERTIES.layout.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.position.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.spacing.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.typography.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.border.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.size.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.effects.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.background.type,
+		COMPONENT_STYLE_GROUP_PROPERTIES.accentColor.type,
+	],
 };
 
 export { propertiesDefinition, stylePropertiesDefinition };
