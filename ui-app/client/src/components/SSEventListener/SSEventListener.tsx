d76affcf06094946e6130c0508779c333a166e5f
serialcoder13 <kiran.grandhi@fincity.com>
2026-01-24 18:05:12 +0530

    Refactor Tabs, Tags, and TextList components to use lazy loading for improved performance
    - Replaced the existing TabsComponent with a LoadLazyTabs component that utilizes React.lazy for dynamic import.
    - Created a new LazyTabs component to encapsulate the original Tabs logic.
    - Updated Tags component to use LoadLazyTags for lazy loading, improving initial load time.
    - Introduced LazyTags component to handle the original Tags functionality.
    - Refactored TextList to use LoadLazyTextList for lazy loading, enhancing performance.
    - Created LazyTextList component to manage the original TextList logic.
    - Removed unused imports and code related to state management and event handling from the original components.

--- ui-app/client/src/components/SSEventListener/SSEventListener.tsx
+++ ui-app/client/src/components/SSEventListener/SSEventListener.tsx
@@ -1,140 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import {
-	getDataFromPath,
-	getPathFromLocation,
-	PageStoreExtractor,
-	setData,
-	UrlDetailsExtractor,
-} from '../../context/StoreContext';
+import React, { Suspense } from 'react';
 import { ComponentProps } from '../../types/common';
-import { runEvent } from '../util/runEvent';
-import useDefinition from '../util/useDefinition';
 import SSEventListenerStyle from './SSEventListenerStyle';
 import { propertiesDefinition, stylePropertiesDefinition } from './sseventListenerProperties';
 import { styleProperties, styleDefaults } from './sseventListenerStyleProperties';
 import { IconHelper } from '../util/IconHelper';
-import { HelperComponent } from '../HelperComponents/HelperComponent';
-import { messageToMaster } from '../../slaveFunctions';
-import { duplicate } from '@fincity/kirun-js';
 
-function SSEventListener(props: Readonly<ComponentProps>) {
-	const {
-		definition,
-		pageDefinition,
-		locationHistory,
-		context,
-		definition: { bindingPath },
-	} = props;
-	const pageExtractor = PageStoreExtractor.getForContext(context.pageName);
-	const urlExtractor = UrlDetailsExtractor.getForContext(context.pageName);
-	const { properties: { url, eventName, onEvent, left = 0, top = 0 } = {} } = useDefinition(
-		definition,
-		propertiesDefinition,
-		stylePropertiesDefinition,
-		locationHistory,
-		pageExtractor,
-		urlExtractor,
-	);
-
-	let bindingPathPath = bindingPath
-		? getPathFromLocation(bindingPath, locationHistory, pageExtractor)
-		: undefined;
-
-	useEffect(() => {
-		if (!bindingPath || !url || !onEvent || !pageDefinition.eventFunctions?.[onEvent]) return;
-
-		const eventFunction = pageDefinition.eventFunctions?.[onEvent];
-		const es = new EventSource(url, { withCredentials: true });
-
-		const func = (event: MessageEvent) => {
-			const data = event.data;
-
-			let value = getDataFromPath(bindingPathPath, locationHistory, pageExtractor);
-
-			value = value ? duplicate(value) : [];
-			value.push(data);
-
-			setData(bindingPathPath!, value, pageExtractor.getPageName());
-
-			if (!eventFunction) return;
-			runEvent(eventFunction, onEvent, context.pageName, locationHistory, pageDefinition);
-		};
-
-		if (Array.isArray(eventName)) {
-			eventName.forEach(name => {
-				es.addEventListener(name, func);
-			});
-		} else if (typeof eventName === 'string') {
-			es.addEventListener(eventName, func);
-		}
-
-		es.onerror = e => console.error('SSE error (auto-retrying)', e);
-
-		return () => es.close();
-	}, [bindingPathPath, url, onEvent, eventName]);
-
-	const ref = useRef<HTMLDivElement>(null);
-
-	return globalThis.designMode ? (
-		<div
-			className="comp compSSEventListener"
-			ref={ref}
-			style={{ transform: `translate(${left}px, ${top}px)` }}
-			onMouseDown={ev => {
-				ev.preventDefault();
-				ev.stopPropagation();
-
-				if (!ref.current || ev.button !== 0) return;
-
-				let startX = ev.clientX;
-				let startY = ev.clientY;
-				let newX = left;
-				let newY = top;
-
-				const mouseUpHandler = (e: MouseEvent) => {
-					e.preventDefault();
-					e.stopPropagation();
-					document.body.removeEventListener('mousemove', mouseMoveHandler);
-					document.body.removeEventListener('mouseup', mouseUpHandler);
-
-					messageToMaster({
-						type: 'SLAVE_COMP_PROP_CHANGED',
-						payload: {
-							key: props.definition.key,
-							properties: [
-								{
-									name: 'left',
-									value: newX,
-								},
-								{
-									name: 'top',
-									value: newY,
-								},
-							],
-						},
-					});
-				};
-
-				const mouseMoveHandler = (e: MouseEvent) => {
-					e.preventDefault();
-					e.stopPropagation();
-					if (!ref.current) return;
-
-					newX = left + e.clientX - startX;
-					newY = top + e.clientY - startY;
-					const style = `translate(${newX}px, ${newY}px)`;
-					ref.current.style.transform = style;
-				};
-
-				document.body.addEventListener('mousemove', mouseMoveHandler);
-				document.body.addEventListener('mouseup', mouseUpHandler);
-			}}
-		>
-			{component.subComponentDefinition[0].icon}
-			<HelperComponent context={context} definition={definition} />
-		</div>
-	) : (
-		<></>
+const LazySSEventListener = React.lazy(
+	() => import(/* webpackChunkName: "SSEventListener" */ './LazySSEventListener'),
+);
+function LoadLazySSEventListener(props: Readonly<ComponentProps>) {
+	return (
+		<Suspense fallback={<>...</>}>
+			<LazySSEventListener {...props} />
+		</Suspense>
 	);
 }
 
@@ -142,7 +20,7 @@ const component = {
 	name: 'SSEventListener',
 	displayName: 'SSEventListener',
 	description: 'SSEventListener component with user-defined function execution',
-	component: SSEventListener,
+	component: LoadLazySSEventListener,
 	styleComponent: SSEventListenerStyle,
 	styleDefaults: styleDefaults,
 	propertyValidation: () => [],
--- ui-app/client/src/components/TextList/TextList.tsx
+++ ui-app/client/src/components/TextList/TextList.tsx
@@ -1,184 +1,26 @@
-import React, { useCallback, useState } from 'react';
-import { PageStoreExtractor, UrlDetailsExtractor } from '../../context/StoreContext';
-import { HelperComponent } from '../HelperComponents/HelperComponent';
+import React, { Suspense } from 'react';
 import { ComponentPropertyDefinition, ComponentProps } from '../../types/common';
-import { getTranslations } from '../util/getTranslations';
 import { propertiesDefinition, stylePropertiesDefinition } from './textListProperties';
 import { Component } from '../../types/common';
 import TextListStyle from './TextListStyle';
-import useDefinition from '../util/useDefinition';
-import { processComponentStylePseudoClasses } from '../../util/styleProcessor';
-import { getRenderData } from '../util/getRenderData';
-import { SubHelperComponent } from '../HelperComponents/SubHelperComponent';
 import { styleProperties, styleDefaults } from './textListStyleProperties';
 import { IconHelper } from '../util/IconHelper';
 
-function TextList(props: Readonly<ComponentProps>) {
-	const {
-		pageDefinition: { translations },
-		definition,
-		locationHistory,
-		context,
-	} = props;
-	const pageExtractor = PageStoreExtractor.getForContext(context.pageName);
-	const urlExtractor = UrlDetailsExtractor.getForContext(context.pageName);
-	const {
-		properties: {
-			text,
-			delimitter,
-			listIcon,
-			listType,
-			listStyleType,
-			labelKey,
-			labelKeyType,
-			uniqueKey,
-			uniqueKeyType,
-			datatype,
-			data,
-			start,
-			reversed,
-		} = {},
-		stylePropertiesWithPseudoStates,
-	} = useDefinition(
-		definition,
-		propertiesDefinition,
-		stylePropertiesDefinition,
-		locationHistory,
-		pageExtractor,
-		urlExtractor,
-	);
-	let dropdownData: any = React.useMemo(
-		() =>
-			getRenderData(
-				data,
-				datatype,
-				uniqueKeyType,
-				uniqueKey,
-				labelKeyType,
-				labelKey,
-				labelKeyType,
-				labelKey,
-			),
-		[data, datatype, uniqueKeyType, uniqueKey, labelKeyType, labelKey, labelKeyType, labelKey],
-	);
-
-	const [hover, setHover] = useState('');
-	const styleHoverProperties = processComponentStylePseudoClasses(
-		props.pageDefinition,
-		{ hover: true },
-		stylePropertiesWithPseudoStates,
-	);
-
-	const styleProperties = processComponentStylePseudoClasses(
-		props.pageDefinition,
-		{ hover: false },
-		stylePropertiesWithPseudoStates,
-	);
-
-	let translatedText = dropdownData?.length
-		? dropdownData.map((e: any) => {
-				return { ...e, label: getTranslations(e.label, translations) };
-			})
-		: text && text.length
-			? text.split(delimitter).map((e: string) => getTranslations(e, translations))
-			: [];
-
-	translatedText = translatedText.map((e: any) => [e.key ?? e, e.label ?? e]);
-
+const LazyTextList = React.lazy(() => import(/* webpackChunkName: "TextList" */ './LazyTextList'));
+function LoadLazyTextList(props: Readonly<ComponentProps>) {
 	return (
-		<div className="comp compTextList" style={styleProperties.comp ?? {}}>
-			<HelperComponent context={props.context} definition={definition} />
-			{listType === 'ol' ? (
-				<ol
-					style={{
-						...(styleProperties.list ?? {}),
-						listStyleType: listStyleType,
-					}}
-					className="list"
-					start={start ? start : undefined}
-					reversed={reversed}
-				>
-					<SubHelperComponent definition={props.definition} subComponentName="list" />
-					{translatedText.map((e: any) => (
-						<li
-							style={
-								(e[0] === hover ? styleProperties : styleHoverProperties)
-									.listItem ?? {}
-							}
-							className="listItem"
-							key={e[0]}
-							onMouseEnter={() =>
-								stylePropertiesWithPseudoStates?.hover && setHover(e[0])
-							}
-							onMouseLeave={() =>
-								stylePropertiesWithPseudoStates?.hover &&
-								e[0] === hover &&
-								setHover('')
-							}
-						>
-							<SubHelperComponent
-								definition={props.definition}
-								subComponentName="listItem"
-							/>
-							{e[1]}
-						</li>
-					))}
-				</ol>
-			) : (
-				<ul
-					style={{
-						...(styleProperties.list ?? {}),
-						listStyleType: listIcon ? 'none' : listStyleType,
-					}}
-				>
-					<SubHelperComponent definition={props.definition} subComponentName="list" />
-					{translatedText.map((e: any) => (
-						<li
-							style={
-								(e[0] === hover ? styleProperties : styleHoverProperties)
-									.listItem ?? {}
-							}
-							className="listItem"
-							key={e[0]}
-							onMouseEnter={() =>
-								stylePropertiesWithPseudoStates?.hover && setHover(e[0])
-							}
-							onMouseLeave={() =>
-								stylePropertiesWithPseudoStates?.hover &&
-								e[0] === hover &&
-								setHover('')
-							}
-						>
-							<SubHelperComponent
-								definition={props.definition}
-								subComponentName="listItem"
-							/>
-							<i
-								style={
-									(e[0] === hover ? styleProperties : styleHoverProperties)
-										.listItemIcon ?? {}
-								}
-								className={`${listIcon} listItemIcon`}
-							>
-								<SubHelperComponent
-									definition={props.definition}
-									subComponentName="listItemIcon"
-								/>
-							</i>
-							{e[1]}
-						</li>
-					))}
-				</ul>
-			)}
-		</div>
+		<Suspense fallback={<>...</>}>
+			<LazyTextList {...props} />
+		</Suspense>
 	);
 }
+
 
 const component: Component = {
 	name: 'TextList',
 	displayName: 'Text List',
 	description: 'TextList component',
-	component: TextList,
+	component: LoadLazyTextList,
 	propertyValidation: (props: ComponentPropertyDefinition): Array<string> => [],
 	properties: propertiesDefinition,
 	styleComponent: TextListStyle,
